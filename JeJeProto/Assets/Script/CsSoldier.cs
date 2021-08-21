272639d5ae577c0dc7bf8835ea5705b13aabe7a9
MetroDefro <65336674+MetroDefro@users.noreply.github.com>
2021-08-22 00:09:41 +0900

    ver.1.45
    AI 대폭 수정

--- JeJeProto/Assets/Script/CsSoldier.cs
+++ JeJeProto/Assets/Script/CsSoldier.cs
@@ -10,8 +10,9 @@ public class CsSoldier : MonoBehaviour
     private Animator animator;
 
     Vector3 PPos;
-    public bool canMove;
+    //public bool canMove;
     public bool founding;
+    public bool time;
 
     // Start is called before the first frame update
     void Start()
@@ -22,48 +23,42 @@ public class CsSoldier : MonoBehaviour
 
         animator.SetBool("Walk", true);
 
-        canMove = false;
         founding = false;
+        time = true;
+
+        // 제제와 객체의 위치 계산
+        if ((player.transform.position.x - transform.position.x) >= (player.transform.position.z - transform.position.z)) //x축이 더 멀면
+        {
+            // 제제 x위치만 가져온 좌표 생성
+            PPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+        else // 반대
+        {
+            // 제제 z위치만 가져온 좌표 생성
+            PPos = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+        }
+        transform.LookAt(PPos); // 일단 이동 방향을 바라봄
     }
 
     // Update is called once per frame
     void Update()
     {
         blocked();
-        if (canMove)
-            Move();
-        else
+        if (founding)
             Direction();
+        Move();
     }
 
     private void Direction()
     {
-        if (founding) // 벽에 막혀서 방향 찾는 중이면
-        {
-            rigidbody.MovePosition(transform.position + transform.forward * 0.256f * Time.deltaTime); // 바라보는 방향 쪽으로 이동
-            return;
-        }
-        // 제제와 객체의 위치 계산
-        if ((player.transform.position.x - transform.position.x) >= (player.transform.position.z - transform.position.z)) //x축이 더 멀면
-        {
-            // 제제 Z위치만 가져온 좌표 생성
-            PPos = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
-        }
-        else // 반대
-        {
-            // 제제 X위치만 가져온 좌표 생성
-            PPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
-        }
+        //임의 회전. 랜덤으로 값을 주도록 해도 괜찮을듯 (90*n)
+        transform.Rotate(new Vector3(0, 90, 0));
 
-        transform.LookAt(PPos); // 일단 이동 방향을 바라봄
-        canMove = true;
+        founding = false;
     }
 
     private void Move()
     {
-       // animator.SetBool("Walk", true); // 걷기 모션 켜기
-
-        
         rigidbody.MovePosition(transform.position + transform.forward * 0.256f * Time.deltaTime); // 바라보는 방향 쪽으로 이동
 
     }
@@ -72,22 +67,36 @@ public class CsSoldier : MonoBehaviour
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.256f))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.128f))
         {
             if (hit.collider.gameObject.tag == "Wall")
-                founding = true;
-                canMove = false;
-                // 콜라이더 막힌 경우 다른 방향으로 이동하게 하는 것 필요
-                // 90도씩 다 돌려보기?
-                transform.rotation = Quaternion.Euler(0, 90, 0);
-                StartCoroutine(Timer());
-
+            {
+                if (time)
+                    StartCoroutine(Timer());
+                else
+                    founding = true;
+            }
         }
     }
     IEnumerator Timer()
     {
-        // 코루틴으로 이렇게 이동하는 것은 한 5초? 10초? 정도로 하고 이거 끝나면 다시 체크해서 움직여보기
-        yield return new WaitForSeconds(2f);
-        founding = false;
+        time = false;
+
+        // 제제와 객체의 위치 계산
+        if ((player.transform.position.x - transform.position.x) >= (player.transform.position.z - transform.position.z)) //x축이 더 멀면
+        {
+            // 제제 x위치만 가져온 좌표 생성
+            PPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+        else // 반대
+        {
+            // 제제 z위치만 가져온 좌표 생성
+            PPos = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+        }
+        transform.LookAt(PPos); // 일단 이동 방향을 바라봄
+
+        // 8초마다 제제 위치 확인해서 시선
+        yield return new WaitForSeconds(8f);
+        time = true;
     }
 }
